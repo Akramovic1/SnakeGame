2f9064df2c5ff86258b38a64ee6c1ea53218fcec
Akramovic1 <eng.akramovic@gmail.com>
2021-09-07 19:48:53 +0200

    Finish V1.0

--- main.py
+++ main.py
@@ -30,11 +30,9 @@ while game_is_on:
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         scoreboard.gameOver()
-    for segment in snake.segments:
-        if segment != snake.head:
-            if snake.head.distance(segment) < 10:
-                game_is_on = False
-                scoreboard.gameOver()
-
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.gameOver()
 
 screen.exitonclick()
